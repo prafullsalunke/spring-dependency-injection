95e19de31ca2305c66b70ddf8347225473ef849c
Prafull Salunke <prafull.salunke@gupshup.io>
2023-11-06 17:36:27 +0530

    Dependency injection : Testing using Autowired annotation

--- src/test/java/prafull/springframework/spring6webappassignment/Spring6WebappAssignmentApplicationTests.java
+++ src/test/java/prafull/springframework/spring6webappassignment/Spring6WebappAssignmentApplicationTests.java
@@ -1,11 +1,28 @@
 package prafull.springframework.spring6webappassignment;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import prafull.springframework.spring6webappassignment.controllers.MyController;
 
 @SpringBootTest
 class Spring6WebappAssignmentApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+	@Test
+	void testAutowireOfConroller() {
+		System.out.println(myController.sayHello());
+	}
+	@Test
+	void testGetControllerFromCtx(){
+		MyController myController = applicationContext.getBean(MyController.class);
+		System.out.println(myController.sayHello());
+	}
 	@Test
 	void contextLoads() {
 	}
